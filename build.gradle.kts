31151397c87cdb954a14a29df17208daba80f243
ofonsecan <o.fonsecan@uniandes.edu.co>
2024-10-27 18:38:03 -0500

    fix plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
     kotlin("plugin.serialization") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
-    id("org.sonarqube") version "5.1.0.4882" apply false
+    id("org.sonarqube") version "4.0.0.2929" apply true
 }
 
 // Apply the SonarQube plugin in the app module where it is needed
@@ -12,10 +12,10 @@ subprojects {
     apply(plugin = "org.sonarqube")
 
     sonarqube {
-        properties {
-            property("sonar.projectKey", "ofonsecan_vinilos-4203-2023-15")
-            property("sonar.organization", "ofonsecan")
-            property("sonar.host.url", "https://sonarcloud.io")
-        }
+    properties {
+        property("sonar.projectKey", "ofonsecan_vinilos-4203-2023-15")
+        property("sonar.organization", "ofonsecan")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
+}
 }
