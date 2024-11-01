f7c5fee377eade0ba01a51dafc1ddb5fddb3b005
Oscar Fonseca <o.fonsecan@uniandes.edu.co>
2024-11-01 13:50:46 -0500

    Use sonarcloud sujest yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,13 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
     kotlin("plugin.serialization") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("org.sonarqube") version "5.1.0.4882"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "ofonsecan_vinilos-4203-2023-15")
+        property("sonar.organization", "ofonsecan")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
